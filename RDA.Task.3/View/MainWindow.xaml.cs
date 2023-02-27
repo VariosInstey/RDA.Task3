2a2db45f32cfc40f3e8bbf91d4f27c0f9596cfd9
VariosInstey <124655869+VariosInstey@users.noreply.github.com>
2023-02-27 16:14:33 +0300

    Добавьте файлы проекта.

---
+++ RDA.Task.3/View/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using RDA.Task._3.Core;
+using RDA.Task._3.View.Pages;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RDA.Task._3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CoreNavigate.MyConnection = MainFrame;
+            MainFrame.Navigate(new MainPage());
+        }
+    }
+}
